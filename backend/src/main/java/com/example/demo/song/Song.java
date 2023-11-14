119760d6d805d8bdbfc91054da99cb3d306dec61
Taro Anklam <s86175@bht-berlin.de>
2023-11-14 19:46:44 +0100

    Unit Tests for Song

--- backend/src/main/java/com/example/demo/song/Song.java
+++ backend/src/main/java/com/example/demo/song/Song.java
@@ -98,13 +98,13 @@ public class Song {
         this.likes = likes;
     }
 
-    // public List<Comment> getComments() {
-    //     return comments;
-    // }
+    public List<Comment> getComments() {
+        return comments;
+    }
 
-    // public void setComments(List<Comment> comments) {
-    //     this.comments = comments;
-    // }
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
 
     public User getUser() {
         return user;
