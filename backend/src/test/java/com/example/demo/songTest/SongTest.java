82260ab5e5a64b88f4b29c3d74103875204a21f5
Taro Anklam <s86175@bht-berlin.de>
2023-11-15 20:46:06 +0100

    new test properties

--- backend/src/test/java/com/example/demo/songTest/SongTest.java
+++ backend/src/test/java/com/example/demo/songTest/SongTest.java
@@ -1,109 +1,107 @@
-// package com.example.demo.songTest;
+package com.example.demo.songTest;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertNotNull;
-// import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-// import java.time.LocalDate;
-// import java.util.List;
+import java.time.LocalDate;
+import java.util.List;
 
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
-// import org.junit.runner.RunWith;
-// import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-// import com.example.demo.comment.Comment;
-// import com.example.demo.song.Song;
-// import com.example.demo.user.User;
+import com.example.demo.comment.Comment;
+import com.example.demo.song.Song;
+import com.example.demo.user.User;
 
-// import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 
-// @RunWith(SpringRunner.class)
-// // @DataJpaTest
-// // @AutoConfigureEmbeddedDatabase
-// @SpringBootTest
-// public class SongTest {
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class SongTest {
     
-//     private Song song;
-//     private User user;
+    private Song song;
+    private User user;
 
-//     @BeforeEach
-//     public void setUp(){
-//         song = new Song(1L,"Sandstorm", "Darude", 360, 10, "Trance");
-//         user = new User("john", "john@doe.com", LocalDate.now());
-//     }
+    @BeforeEach
+    public void setUp(){
+        song = new Song(1L,"Sandstorm", "Darude", 360, 10, "Trance");
+        user = new User("john", "john@doe.com", LocalDate.now());
+    }
 
-//     @Test
-//     public void testCorrectValues() {
-//         assertNotNull(song);
-//         assertEquals(Long.valueOf(1), song.getId());
-//         assertEquals("Sandstorm", song.getTitle());
-//         assertEquals("Darude", song.getActor());
-//         assertEquals(Integer.valueOf(360), song.getSongDuration());
-//         assertEquals(Integer.valueOf(10), song.getLikes());
-//         assertEquals("Trance", song.getGenre());
-//     }
+    @Test
+    public void testCorrectValues() {
+        assertNotNull(song);
+        assertEquals(Long.valueOf(1), song.getId());
+        assertEquals("Sandstorm", song.getTitle());
+        assertEquals("Darude", song.getActor());
+        assertEquals(Integer.valueOf(360), song.getSongDuration());
+        assertEquals(Integer.valueOf(10), song.getLikes());
+        assertEquals("Trance", song.getGenre());
+    }
 
-//     @Test
-//     public void testGetterAndSetter(){
-//         song.setId(2L);
-//         assertEquals(2L, song.getId());
+    @Test
+    public void testGetterAndSetter(){
+        song.setId(2L);
+        assertEquals(2L, song.getId());
 
-//         song.setTitle("Money Trees");
-//         assertEquals("Money Trees", song.getTitle());
+        song.setTitle("Money Trees");
+        assertEquals("Money Trees", song.getTitle());
 
-//         song.setActor("Kendrick Lamar");
-//         assertEquals("Kendrick Lamar", song.getActor());
+        song.setActor("Kendrick Lamar");
+        assertEquals("Kendrick Lamar", song.getActor());
 
-//         song.setSongDuration(300);
-//         assertEquals(Integer.valueOf(300), song.getSongDuration());
+        song.setSongDuration(300);
+        assertEquals(Integer.valueOf(300), song.getSongDuration());
 
-//         song.setLikes(12);
-//         assertEquals(Integer.valueOf(12), song.getLikes());
+        song.setLikes(12);
+        assertEquals(Integer.valueOf(12), song.getLikes());
 
-//         song.setGenre("Rap");
-//         assertEquals("Rap", song.getGenre());
-//     }
+        song.setGenre("Rap");
+        assertEquals("Rap", song.getGenre());
+    }
 
-//     @Test
-//     public void testComments(){
-//         List<Comment> comments = List.of(new Comment("Test 1", user, song), new Comment("Test 2", user, song));
+    @Test
+    public void testComments(){
+        List<Comment> comments = List.of(new Comment("Test 1", user, song), new Comment("Test 2", user, song));
 
-//         song.setComments(comments);
+        song.setComments(comments);
 
-//         assertNotNull(song.getComments());
+        assertNotNull(song.getComments());
         
-//         assertEquals(2, song.getComments().size());
+        assertEquals(2, song.getComments().size());
         
-//         assertEquals("Test 1", song.getComments().get(0).getText());
-//         assertEquals(user, song.getComments().get(0).getCreator());
+        assertEquals("Test 1", song.getComments().get(0).getText());
+        assertEquals(user, song.getComments().get(0).getCreator());
 
-//         assertEquals("Test 2", song.getComments().get(1).getText());
-//         assertEquals(user, song.getComments().get(1).getCreator());
-//     }
+        assertEquals("Test 2", song.getComments().get(1).getText());
+        assertEquals(user, song.getComments().get(1).getCreator());
+    }
 
-//     @Test
-//     public void testUserAssociation() {
+    @Test
+    public void testUserAssociation() {
         
-//         assertNull(song.getUser());
+        assertNull(song.getUser());
 
-//         song.setUser(user);
+        song.setUser(user);
 
-//         assertNotNull(song.getUser());
-//         assertEquals("john", song.getUser().getName());
-//     }
+        assertNotNull(song.getUser());
+        assertEquals("john", song.getUser().getName());
+    }
 
-//     @Test
-//     public void testToString(){
-//         assertEquals(
-//             "Song [id=" + song.getId() + ", title=" + song.getTitle() 
-//                 + ", actor="  + song.getActor() + ", songDuration=" + song.getSongDuration() 
-//                 + ", likes=" + song.getLikes() 
-//                 + ", user=" + song.getUser()
-//                 + ", genre=" + song.getGenre() + "]", 
-//             song.toString()
-//         );
-//     }
-// }
+    @Test
+    public void testToString(){
+        assertEquals(
+            "Song [id=" + song.getId() + ", title=" + song.getTitle() 
+                + ", actor="  + song.getActor() + ", songDuration=" + song.getSongDuration() 
+                + ", likes=" + song.getLikes() 
+                + ", user=" + song.getUser()
+                + ", genre=" + song.getGenre() + "]", 
+            song.toString()
+        );
+    }
+}
